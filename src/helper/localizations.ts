de6aa81d0a9ab9f8266ff51921cef6b054df96c2
amir najafi <amir.najafi66@gmail.com>
2023-07-05 15:51:55 +0300

    add localizations

---
+++ src/helper/localizations.ts
@@ -0,0 +1,10 @@
+import LocalizedStrings from "react-localization";
+import en from "../assets/locales/en.json";
+import fa from "../assets/locales/fa.json";
+
+const Strings = new LocalizedStrings({
+  en: en,
+  fa: fa,
+});
+
+export default Strings;
